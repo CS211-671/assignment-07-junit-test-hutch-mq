46edf9954b2fe6e8c86daf5e30aa97e9870ff4fc
Aueaaungkool <Aueaaungkool.s@ku.th>
2024-08-22 16:53:07 +0700

    usertest1

--- src/test/java/ku/cs/models/UserListTest.java
+++ src/test/java/ku/cs/models/UserListTest.java
@@ -2,6 +2,7 @@ package ku.cs.models;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.lang.String;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,30 +12,48 @@ class UserListTest {
     @DisplayName("User should be found in UserList")
     public void testUserListFindUser() {
         // TODO: add 3 users to UserList
+        UserList users = new UserList();
+        users.addUser("a1", "1");
+        users.addUser("b1", "2");
+        users.addUser("c1", "3");
 
         // TODO: find one of them
+        User user = users.findUserByUsername("a1");
 
         // TODO: assert that UserList found User
-        // String expected = "<one of username>";
-        // String actual = user.getUsername();
-        // assertEquals(expected, actual);
+        //String expected = "<one of username>";
+
+        String expected = "a1";
+        String actual = user.getUsername();
+        assertEquals(expected, actual);
     }
+
 
     @Test
     @DisplayName("User can change password")
     public void testUserCanChangePassword() {
         // TODO: add 3 users to UserList
+        UserList users = new UserList();
+        users.addUser("a1", "1");
+        users.addUser("b1", "2");
+        users.addUser("c1", "3");
 
         // TODO: change password of one user
+        //User user = user.validatePassword("1");
 
         // TODO: assert that user can change password
-        // assertTrue(actual);
+        String actual = user.getPassword();
+        assertTrue(actual);
     }
 
     @Test
     @DisplayName("User with correct password can login")
     public void testUserListShouldReturnObjectIfUsernameAndPasswordIsCorrect() {
         // TODO: add 3 users to UserList
+        UserList users = new UserList();
+        users.addUser("a1", "1");
+        users.addUser("b1", "2");
+        users.addUser("c1", "3");
 
         // TODO: call login() with correct username and password
 
@@ -46,6 +65,10 @@ class UserListTest {
     @DisplayName("User with incorrect password cannot login")
     public void testUserListShouldReturnNullIfUsernameAndPasswordIsIncorrect() {
         // TODO: add 3 users to UserList
+        UserList users = new UserList();
+        users.addUser("a1", "1");
+        users.addUser("b1", "2");
+        users.addUser("c1", "3");
 
         // TODO: call login() with incorrect username or incorrect password
 
